562d2e5ee6eb655a7e37636388ada619fe4aefda
Débora Inácio <66495786+Debs28@users.noreply.github.com>
2022-06-05 10:51:20 -0300

    Add files via upload

---
+++ Projeto Sustentabilidade/Menu/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    window.location = "../PaginaInicial/PaginaInicial.html";
+}
+
+function Projetos() {
+    window.location = "../Projeto/projetos.html";
+}
+
+function Reciclagem() {
+    window.location = "../Reciclagem/reciclagem.html";
+}
---
+++ Projeto Sustentabilidade/Reciclagem/functionButton.js
@@ -0,0 +1,15 @@
+function IdeiasPapel() {
+    window.location = "./ideias/papel.html";
+}
+
+function IdeiasPlastico() {
+    window.location = "./ideias/plastico.html";
+}
+
+function IdeiasVidro() {
+    window.location = "./ideias/vidro.html";
+}
+
+function IdeiasMetal() {
+    window.location = "./ideias/metal.html";
+}
